514d3c62a125f15790b2a885a0f4cd5ccb552312
munish259 <munish.peenith@city.ac.uk>
2021-12-27 20:10:25 +0000

    comment push
    previous push now with comments

--- code.py
+++ code.py
@@ -28,18 +28,20 @@ myFile2.isnull().any()
 # removing and uneeded comlumns
 #myFile = myFile2.drop('id', axis=1)
 
-print(myFile2.head())
+#print(myFile2.head())
 
 # defining dependent and independent variables
 X = myFile2[['battery_power', 'blue', 'clock_speed', 'dual_sim', 'fc', 'four_g', 'int_memory', 'm_dep', 'mobile_wt',
       'n_cores', 'pc', 'px_height', 'px_width', 'ram', 'sc_h', 'sc_w', 'talk_time', 'three_g', 'touch_screen', 'wifi']]
 y = myFile2['price_range']
 
-
+# splitting the data into training and testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.25, random_state=42)
+
+# building the model
 linearModel = LogisticRegression()
 linearModel.fit(X_train, y_train)
 y_pred = linearModel.predict(X_test)
+
+# printing the classification report
 print(classification_report(y_test, y_pred))
-
-
