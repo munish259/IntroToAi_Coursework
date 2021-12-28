615f4cdb6a6bd54ae0f53837672c096c3b888b3f
munish259 <munish.peenith@city.ac.uk>
2021-12-28 03:22:37 +0000

    Analysis debug
    Spyder check.

--- code.py
+++ code.py
@@ -5,18 +5,23 @@ import pandas as pd
 import matplotlib.pyplot as plt  
 from sklearn.model_selection import train_test_split 
 
-
+from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 
 from sklearn.metrics import classification_report
-
 from sklearn import metrics
-
-import matplotlib.pyplot as plt
 from sklearn.metrics import plot_confusion_matrix
+from scipy.stats import zscore
+import seaborn as sns
 
+
+path = "."  #absolute or relative path to the folder containing the file. 
+            #"." for current folder
+
+filename_read = os.path.join(path, "train.csv")
+df = pd.read_csv(filename_read) # for analysis
 
 
 myFile2 = pd.read_csv('train.csv')
@@ -42,7 +47,61 @@ y = myFile2['price_range']
 # splitting the data into training and testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.25, random_state=42)
 
+# function for intro analysis - please run using spyder
 
+def analyse_data():
+      path = "."  #absolute or relative path to the folder containing the file. 
+                #"." for current folder
+    
+      filename_read = os.path.join(path, "train.csv")
+      df = pd.read_csv(filename_read) # for analysis
+
+      '''Start of analysis of data'''
+      # Strip non-numerics
+      df = df.select_dtypes(include=['int', 'float'])
+      
+      print  ("\nCounting of data: \n")
+      
+      #details on the data in numbers 
+      for col in df.columns:
+          print(col + '\n_____________')
+          print(df[col].value_counts())
+          print('_____________________________\n')
+
+      
+      print("\nStatistics: \n")
+
+      #display statistics 
+      get_stats()
+
+      #visualise data so that it is clear what the data is showing
+      df.hist(figsize=(20,20))
+      plt.show()
+      #sns.pairplot(df,hue='price_range')
+
+      #heat map isa correlation matrix used to show the correlations between each field 
+      plt.figure(figsize=(16,16))
+      sns.heatmap(df.corr(), annot=True, fmt=".2f");
+      '''End of analysis of data'''
+      
+      
+def get_stats():
+    
+      headers = list(df.columns.values)
+      fields = []
+      
+      # Perform basic statistics (mean, variance, standard deviation, z scores) on a dataframe.
+      for field in headers:
+          fields.append({
+              'name' : field,
+              'mean': df[field].mean(),
+              'var': df[field].var(),
+              'sdev': df[field].std(),# how dispersed the data is in relation to the mean
+
+          })
+      #display statistics 
+      for field in fields:
+          print(field)
 
 #function for ease of access
 def logistic():
@@ -115,9 +174,30 @@ def SVM():
       plt.show()
 
       #ref: https://analyticsindiamag.com/understanding-the-basics-of-svm-with-example-and-python-implementation/
+      
+def nb():
+
+      # building the model
+      nb = GaussianNB()
+      nb.fit(X_train,y_train)
+
+      # making predictions
+      y_pred = nb.predict(X_test)
+      
+      #prints accuracy using metrics
+      print("\n (metric) Accuracy: ", metrics.accuracy_score(y_test, y_pred), "\n")
+      
+      # printing the classification report 
+      print(classification_report(y_test,y_pred))
+
+      # plotting and printing confusion matrix
+      plot_confusion_matrix(nb, X_test,y_pred)
+      plt.show()
 
 
+analyse_data()
 
 #logistic()
 #decisionTree()
-SVM()
+#SVM()
+#nb()
