2f1b49a0a5a69590530fb102644b4d75d2c5882a
munish259 <munish.peenith@city.ac.uk>
2021-12-28 05:43:10 +0000

    push 1.4
    added kNear/neighbor. General code cleanup.

--- code.py
+++ code.py
@@ -5,10 +5,13 @@ import pandas as pd
 import matplotlib.pyplot as plt  
 from sklearn.model_selection import train_test_split 
 
+
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
 
 from sklearn.metrics import classification_report
 from sklearn import metrics
@@ -65,44 +68,20 @@ def analyse_data():
       print("\nStatistics: \n")
 
       #display statistics 
-      get_stats()
+      get_stats(df)
 
       #visualise data so that it is clear what the data is showing
       df.hist(figsize=(20,20))
       plt.show()
       #sns.pairplot(df,hue='price_range')
 
-      
-def analyse_data():
-      '''Start of analysis of data'''
-      # Strip non-numerics
-      df = df.select_dtypes(include=['int', 'float'])
-
-      headers = list(df.columns.values)
-      fields = []
-      
-      #details on the data in numbers 
-      for col in df.columns:
-          print(col + '\n_____________')
-          print(df[col].value_counts())
-          print('_____________________________\n')
-
-      #display statistics 
-      get_stats()
-
-      #visualise data so that it is clear what the data is showing
-      df.hist(figsize=(20,20))
-      plt.show()
-      #sns.pairplot(df,hue='price_range')
-      
-      #heat map isa correlation matrix used to show the correlations between each field 
+        #heat map isa correlation matrix used to show the correlations between each field 
       plt.figure(figsize=(16,16))
-      sns.heatmap(df.corr(), annot=True, fmt=".2f");
+      sns.heatmap(df.corr(), annot=True, fmt=".2f")
       '''End of analysis of data'''
-
       
       
-def get_stats():
+def get_stats(df):
     
       headers = list(df.columns.values)
       fields = []
@@ -125,9 +104,11 @@ def get_stats():
 
 #function for ease of access
 def logistic():
+    
+      print ("\nLogistic Regression:\n")
 
       # building the model
-      logisticModel = LogisticRegression()
+      logisticModel = LogisticRegression(max_iter = 1000)
       logisticModel.fit(X_train, y_train)
 
       # making our predicitons
@@ -148,7 +129,9 @@ def logistic():
 
 
 def decisionTree():
+        
 
+      print("\nDecision Tree:\n")
 
       # building the model
       clf = DecisionTreeClassifier()
@@ -166,22 +149,23 @@ def decisionTree():
       # plotting and printing confusion matrix
       plot_confusion_matrix(clf, X_test, y_test)  
       plt.show()
-
       
       #reference: https://stackabuse.com/decision-trees-in-python-with-scikit-learn/
 
 
+
 #visualise decision tree in itself
 
-
 def SVM():
+        
+      print("\nSVM:\n")
 
       # building the model
-      regressor  = SVC(kernel='rbf', random_state = 1)
-      regressor.fit(X_train,y_train)
+      sv  = SVC(kernel='rbf', random_state = 1)
+      sv.fit(X_train,y_train)
 
       #making our prediction
-      y_pred = regressor.predict(X_test)
+      y_pred = sv.predict(X_test)
 
       #prints accuracy using metrics
       print("\n (metric) Accuracy: ", metrics.accuracy_score(y_test, y_pred), "\n")
@@ -190,14 +174,17 @@ def SVM():
       print(classification_report(y_test, y_pred))
 
       # plotting and printing confusion matrix
-      plot_confusion_matrix(regressor, X_test, y_test)  
+      plot_confusion_matrix(sv, X_test, y_test)  
       plt.show()
 
       #ref: https://analyticsindiamag.com/understanding-the-basics-of-svm-with-example-and-python-implementation/
       
+
       
 def nb():
-  
+       
+      print("\nNaive Bayes:\n")
+
       # building the model
       nb = GaussianNB()
       nb.fit(X_train,y_train)
@@ -215,12 +202,36 @@ def nb():
       plot_confusion_matrix(nb, X_test,y_pred)
       plt.show()
 
-      
-analyse_data()
 
+
+def kNear():
+
+      print("\nK-neighbor:\n")
+
+      # building the model
+      neigh = KNeighborsClassifier(n_neighbors=3)
+      neigh.fit(X_train, y_train)
+
+      # making predictions
+      y_pred = neigh.predict(X_test)
+
+      #prints accuracy using metrics
+      print("\n (metric) Accuracy: ", metrics.accuracy_score(y_test, y_pred), "\n")
+      
+      # printing the classification report 
+      print(classification_report(y_test,y_pred))
+
+      # plotting and printing confusion matrix
+      plot_confusion_matrix(neigh, X_test,y_pred)
+      plt.show() 
+
+
+      
 #analyse_data()
-#logistic()
-#decisionTree()
-#SVM()
-#nb()
+
+#logistic()             #70%  (rounded) 
+#decisionTree()         #79%  (rounded) 
+#SVM()                  #94%  (rounded) 
+#nb()                   #78%  (rounded) 
+#kNear()                #90%  (rounded)            
 
